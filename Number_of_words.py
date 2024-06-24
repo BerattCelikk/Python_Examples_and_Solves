d6bb3198f5b52ba15f9b0d6125d51c7281020315
Berat Erol CELIK <beratcelikk02@gmail.com>
2024-06-24 17:45:21 +0300

    Number_of_words.py

---
+++ Number_of_words.py
@@ -0,0 +1,6 @@
+#  PROGRAM THAT FINDS THE NUMBER OF TOTAL WORDS IN A SENTENCE
+sentence=input("enter a text :")
+words=sentence.split()
+len_words=len(words)
+
+print("total words in a sentece => ",len_words)
